17f928892d92291e68cf3b58e468aede13a7e385
Pedro Augusto <pedroaugusto.rm2004@gmail.com>
2025-07-01 18:11:20 -0300

    DepartmentDao implementation and test

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,7 +15,7 @@ public class Program {
         
         Scanner sc = new Scanner(System.in);
 
-        SellerDao sellerDao = DaoFactory.creatSellerDao();
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
